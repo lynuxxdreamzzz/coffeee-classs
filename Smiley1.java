b8611c145b96ec8131f2c4352503ee4db59abc84
lynuxxdreamzzz <101957171+lynuxxdreamzzz@users.noreply.github.com>
2022-08-25 15:39:41 -0500

    .

--- Smiley1.java
+++ Smiley1.java
@@ -1,6 +1,6 @@
 public class Smiley1{
     public static void main(String[] args){
-        System.out.println("    _.-::::-._");
+        System.out.println("    _.-\"\"\"\"-._");
         System.out.println("  .':::::::::::.");
         System.out.println(" /::::::::::::::\\");
         System.out.println("|:::(o)::::(o):::|");
