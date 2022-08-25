1a5786131f63522d16592c31f7eab8f552e80908
lynuxxdreamzzz <101957171+lynuxxdreamzzz@users.noreply.github.com>
2022-08-25 15:37:38 -0500

    .

---
+++ Smiley1.java
@@ -0,0 +1,13 @@
+public class Smiley1{
+    public static void main(String[] args){
+        System.out.println("_.-::::-._");
+        System.out.println(".':::::::::::.");
+        System.out.println("/::::::::::::::\\");
+        System.out.println("|:::(o)::::(o):::|");
+        System.out.println("|::::::::::::::::|");
+        System.out.println("|:::\\::::::::/:::|");
+        System.out.println("\\:::`.____.':::/");
+        System.out.println("`.::::::::::.'");
+        System.out.println("``-....-''");
+    }
+}
