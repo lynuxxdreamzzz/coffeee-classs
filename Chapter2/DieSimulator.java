a1e342cd74fa2a9441a6c21111df105c4abbf6c5
lynuxxdreamzzz <101957171+lynuxxdreamzzz@users.noreply.github.com>
2022-08-30 13:32:36 -0700

    .

---
+++ Chapter2/DieSimulator.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public class DieSimulator{
+    public static void main(String[] args){
+        Random die=new Random();
+        System.out.println(die.nextInt(6)+1);
+    }
+}
